ceb8e6f01fcb32bcff43a2efe9b8cfbedbdb7e3e
Greyhatanit <chaudharyanit07@gmail.com>
2020-05-14 09:50:49 +0545

    Second Commit - Error in the context resolved

--- app/src/main/java/com/asyantech/ubamgo/seatbooking/ChooseSeatActivity.java
+++ app/src/main/java/com/asyantech/ubamgo/seatbooking/ChooseSeatActivity.java
@@ -16,11 +16,12 @@ import com.asyantech.ubamgo.seatbookingUtils.AirplaneAdapter;
 import com.asyantech.ubamgo.seatbookingUtils.CenterItem;
 import com.asyantech.ubamgo.seatbookingUtils.EdgeItem;
 import com.asyantech.ubamgo.seatbookingUtils.EmptyItem;
+import com.asyantech.ubamgo.seatbookingUtils.OnSeatSelected;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ChooseSeatActivity extends AppCompatActivity {
+public class ChooseSeatActivity extends AppCompatActivity implements OnSeatSelected {
 
     private static final int COLUMNS = 5;
     private TextView txtSeatSelected;
@@ -33,5 +34,36 @@ public class ChooseSeatActivity extends AppCompatActivity {
 
         txtSeatSelected = (TextView)findViewById(R.id.txt_seat_selected);
         submitCloud = (Button) findViewById(R.id.submit_cloud);
+
+        List<AbstractItem> items = new ArrayList<>();
+        for (int i=0; i<30; i++) {
+            if (i%COLUMNS==0 || i%COLUMNS==4) {
+                items.add(new EdgeItem(String.valueOf(i)));
+            } else if (i%COLUMNS==1 || i%COLUMNS==3) {
+                items.add(new CenterItem(String.valueOf(i)));
+            } else {
+                items.add(new EmptyItem(String.valueOf(i)));
+            }
+        }
+
+        GridLayoutManager manager = new GridLayoutManager(this, COLUMNS);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.lst_items);
+        recyclerView.setLayoutManager(manager);
+
+        AirplaneAdapter adapter = new AirplaneAdapter(this, items);
+        recyclerView.setAdapter(adapter);
+
+        submitCloud.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ChooseSeatActivity.this, "Submit Cloud Button", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onSeatSelected(int count) {
+
+        txtSeatSelected.setText("Book "+count+" seats");
     }
 }
